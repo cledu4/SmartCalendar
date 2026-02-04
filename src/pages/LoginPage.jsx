a0c1abd0050e49b66bb6ba74ebb09e290127fff4
cledu4 <163782250+cledu4@users.noreply.github.com>
2026-02-04 17:41:36 +0100

    Update link LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -57,7 +57,7 @@ function LoginPage() {
           </button>
         </form>
         <p>
-          Pas de compte ? <a href="/signup">Inscris-toi</a>
+          Pas de compte ? <Link to="/signup">Inscris-toi</Link>
         </p>
       </div>
     </div>
